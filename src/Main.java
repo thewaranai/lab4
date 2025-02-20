16310c34aea40081a5c28d2253a2310670919a07
Waranai <waflyaswarenyem@mail.ru>
2025-02-20 09:43:45 +0300

    change method

--- src/Main.java
+++ src/Main.java
@@ -11,28 +11,51 @@ public class Main {
         System.out.println("Массив до сортировки:");
         printArray(array);
 
-        bubbleSort(array);
+        quickSort(array, 0, array.length -1);
 
         System.out.println("Массив после сортировки:");
         printArray(array);
 
        int[] nonZeroArray = delZero(array);
-       bubbleSort(nonZeroArray);
+        quickSort(nonZeroArray, 0, nonZeroArray.length -1);
 
         System.out.println("Массив без нулей:");
         printArray(nonZeroArray);
     }
-    public static void bubbleSort(int[] array) {
-        int n = array.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            // Индекс опорного элемента
+            int pivotIndex = partition(array, low, high);
+
+            // Рекурсивная сортировка левой и правой частей
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    // Метод для разделения массива
+    private static int partition(int[] array, int low, int high) {
+        // Выбор опорного элемента (в данном случае последний элемент)
+        int pivot = array[high];
+        int i = (low - 1); // Индекс меньшего элемента
+
+        for (int j = low; j < high; j++) {
+            // Если текущий элемент меньше или равен опорному
+            if (array[j] <= pivot) {
+                i++;
+                // Обмен array[i] и array[j]
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
             }
         }
+
+        // Обмен array[i + 1] и array[high] (или опорный элемент)
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1; // Возвращаем индекс опорного элемента
     }
     public static int[] delZero(int[] array) {
         int count = 0;
